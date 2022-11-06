5093c38e8f95f1eaa390d10ca78b4cb8ec1813a0
Takuya Matsuda <hilcrhyme.t.toc.katsu@gmail.com>
2022-11-06 18:28:24 +0900

    Update hardhat.config.ts

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,23 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
-import secret from "./secret.json";
 
 const config: HardhatUserConfig = {
-  gasReporter: {
-    enabled: true,
-    currency: "JPY",
-    gasPriceApi: "https://api.etherscan.io/api?module=proxy&action=eth_gasPrice",
-    coinmarketcap: secret.gasapi,
-  },
-  etherscan: {
-    apiKey: secret.etherscan 
-  },
-  networks: {
-    goerli: {
-      url: secret.api,
-      accounts: [secret.privatekey],
-    },
-  },
   solidity: "0.8.17",
   paths: {
     artifacts: "./artifacts"
